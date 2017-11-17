d4d14a504aaaf057b6bd63262316091c6f4b9ac0
Василий Нестеров <da3x11@gmail.com>
2017-11-17 08:35:53 +0300

    Добавил документации

--- application/common/lessons.py
+++ application/common/lessons.py
@@ -8,6 +8,10 @@ from itertools import takewhile
 
 
 class DefaultLesson(namedtuple("DefaultLesson", ["date", "status"])):
+    u"""
+        Структура для сохранения интерфейса класса
+        application.models.Lessons
+    """
 
     def __json__(self):
         return dict(
@@ -16,11 +20,40 @@ class DefaultLesson(namedtuple("DefaultLesson", ["date", "status"])):
         )
 
 
+# Структура для объединения
+# application.models.Students и application.models.PassTypes
+PassTypeStudent = namedtuple("PassTypeStudent", ["student", "pass_type"])
+
+
 def get_students_lessons(group, date, students):
+    u"""
+        Функция для получения списка уроков для заданного
+        списка учеников
+
+        args:
+            group application.models.Groups
+            date datetime.datetime
+            students [application.models.Students]
+
+        return:
+            [application.models.Lessons or DefaultLesson]
+    """
 
     dates = takewhile(
-        lambda x: x.month==date.month,
+        lambda x: x.month == date.month,
         get_calendar(date, group.days)
     )
 
     return (DefaultLesson(date, -2) for date in dates)
+
+
+def create_new_passes(group, date, pt_data):
+    u"""
+        Функция для создания и сохранения абонементов в БД
+
+        args:
+            group application.models.Groups
+            data datetime.datetime
+            pd_data PassTypeStudent
+    """
+    pass
