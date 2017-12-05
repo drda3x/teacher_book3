0415831fde437e8cdfcd31f82258dc55955cd691
Vasily Nesterov <da3x11@gmail.com>
2017-12-05 16:12:58 +0300

    Побеждены gulp-таски для сборки js. CSS, ты следующий! =)))

--- application/static/gulpfile.js
+++ application/static/gulpfile.js
@@ -2,39 +2,28 @@
 
 var gulp = require("gulp");
 var concat = require("gulp-concat");
-var bower = require("gulp-bower-files");
-var gulpFilter = require("gulp-filter");
 var rigger = require("gulp-rigger");
 
 
-//
-// concat *.js to `vendor.js`
-// and *.css to `vendor.css`
-// rename fonts to `fonts/*.*`
-//
-gulp.task('bower', function() {
-  var jsFilter = gulpFilter('**/*.js')
-  var cssFilter = gulpFilter('**/*.css')
-  return bower()
-    .pipe(jsFilter)
-    .pipe(concat('vendor.js'))
-    .pipe(gulp.dest('build'))
-    //.pipe(jsFilter.restore())
-    .pipe(cssFilter)
-    .pipe(concat('vendor.css'))
-    .pipe(gulp.dest('build'))
-    //.pipe(cssFilter.restore())
-    //.pipe(rename(function(path) {
-    //  if (~path.dirname.indexOf('fonts')) {
-    //    path.dirname = '/fonts'
-    //  }
-    //}))
-    //.pipe(gulp.dest(dist.vendor))
+gulp.task('vendor', function() {
+    gulp.src([
+        "bower_components/angular/angular.js",
+        "bower_components/angular-route/angular-route.js",
+        "bower_components/jquery/dist/jquery.js",
+        "bower_components/bootstrap/dist/js/bootstrap.js",
+        "bower_components/angularjs-datepicker/dist/angular-datepicker.js",
+        "bower_components/alertifyjs/dist/js/alertify.js"
+    ]).pipe(concat("vendor.js"))
+        .pipe(gulp.dest("build"));
 });
 
 
 gulp.task('js', function() {
+    gulp.src('src/js/vendor.js')
+        .pipe(concat())
+        .pipe(gulp.dest('build'));
+
     gulp.src('src/js/app.js')
         .pipe(rigger())
-        .pipe(gulp.dest('build'))
+        .pipe(gulp.dest('build'));
 });
