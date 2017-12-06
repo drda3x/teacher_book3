e4a0039121c2fe24e69d18fab52d3be1346d0c0e
Vasily Nesterov <da3x11@gmail.com>
2017-12-06 16:36:58 +0300

    УРЯЯЯ! Победиль сборку css

--- application/static/gulpfile.js
+++ application/static/gulpfile.js
@@ -3,27 +3,39 @@
 var gulp = require("gulp");
 var concat = require("gulp-concat");
 var rigger = require("gulp-rigger");
+var concatCss = require("gulp-concat-css");
 
 
-gulp.task('vendor', function() {
+gulp.task('vendor:js', function() {
     gulp.src([
         "bower_components/angular/angular.js",
         "bower_components/angular-route/angular-route.js",
         "bower_components/jquery/dist/jquery.js",
-        "bower_components/bootstrap/dist/js/bootstrap.js",
+        "node_modules/bootstrap/dist/js/bootstrap.bundle.js",
         "bower_components/angularjs-datepicker/dist/angular-datepicker.js",
-        "bower_components/alertifyjs/dist/js/alertify.js"
+        "bower_components/alertifyjs/src/js/alertify.js"
     ]).pipe(concat("vendor.js"))
         .pipe(gulp.dest("build"));
 });
 
 
-gulp.task('js', function() {
-    gulp.src('src/js/vendor.js')
-        .pipe(concat())
-        .pipe(gulp.dest('build'));
+gulp.task("vendor:css", function() {
+    gulp.src([
+        "node_modules/bootstrap/dist/css/bootstrap.css",
+        "bower_components/alertifyjs/dist/css/alertify.css",
+        "bower_components/angularjs-datepicker/dist/angular-datepicker.css"
+    ]) .pipe(concatCss("vendor.css"))
+    .pipe(gulp.dest('build'));
+});
 
+
+gulp.task('js', function() {
     gulp.src('src/js/app.js')
         .pipe(rigger())
         .pipe(gulp.dest('build'));
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch('src/js/modules/*.js', ['js']);
 });
