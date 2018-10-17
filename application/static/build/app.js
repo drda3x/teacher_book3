d9d201072364038976483fe536131065cd1fb134
Vasily Nesterov <da3x11@gmail.com>
2018-10-17 22:55:29 +0300

    Добавлено поведение кнопок с записями

--- application/static/build/app.js
+++ application/static/build/app.js
@@ -1435,6 +1435,48 @@
             this.selectedMenu = elem;
         }
     
+        $scope.inAddProc = false;
+        $scope.addRecord = function(action) {
+            if(action == 'sampo-menu-add') {
+                this.inAddProc = true;
+            } else if (action == 'sampo-menu-writeoff') {
+                this.inAddProc = true;
+            }
+        }
+    
+        $scope.payments = [];
+        // Добавление оплаты
+        $scope.addPayment = function(time, amount) {
+            this.payments.push({
+                time: time,
+                amount: amount
+            })
+        }
+        
+        $scope.withdrawals = [];
+        // Добавление списания
+        $scope.addWriteoff = function(time, amount, reason) {
+            $scope.withdrawals.push({
+                time: time,
+                amount: amount,
+                reason: reason
+            });
+        }
+        
+        $scope.passes = []; 
+        // Добавление абонементов
+        $scope.addPass = function(time, amount, name) {
+            $scope.payments.push({
+                time: time,
+                amount: amount
+            });
+    
+            $scope.passes.push({
+                name: name,
+                checked: true
+            });
+        }
+    
         $scope.selectMenu('sampo-menu-add');
     })
 })()
--- application/static/src/js/modules/sampo.js
+++ application/static/src/js/modules/sampo.js
@@ -9,5 +9,47 @@ app.controller('sampoCtrl', function($scope) {
         this.selectedMenu = elem;
     }
 
+    $scope.inAddProc = false;
+    $scope.addRecord = function(action) {
+        if(action == 'sampo-menu-add') {
+            this.inAddProc = true;
+        } else if (action == 'sampo-menu-writeoff') {
+            this.inAddProc = true;
+        }
+    }
+
+    $scope.payments = [];
+    // Добавление оплаты
+    $scope.addPayment = function(time, amount) {
+        this.payments.push({
+            time: time,
+            amount: amount
+        })
+    }
+    
+    $scope.withdrawals = [];
+    // Добавление списания
+    $scope.addWriteoff = function(time, amount, reason) {
+        $scope.withdrawals.push({
+            time: time,
+            amount: amount,
+            reason: reason
+        });
+    }
+    
+    $scope.passes = []; 
+    // Добавление абонементов
+    $scope.addPass = function(time, amount, name) {
+        $scope.payments.push({
+            time: time,
+            amount: amount
+        });
+
+        $scope.passes.push({
+            name: name,
+            checked: true
+        });
+    }
+
     $scope.selectMenu('sampo-menu-add');
 })
